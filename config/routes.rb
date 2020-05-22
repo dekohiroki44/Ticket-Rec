2ffbff618778eba5aaee4cb7482526a19a71d579
dekohiroki44 <drinkingrider@hotmail.co.jp>
2020-05-22 10:26:34 +0900

    headerの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    # get 'signup', to: 'devise/registrations#new'
+    # post 'signup', to: 'devise/registrations#create'
+    # get 'login', to: 'devise/sessions#new'
+    # post 'login', to: 'devise/sessions#create'
+    # delete 'logout', to: 'devise/sessions#destroy'
+  end
   root to: "static_pages#home"
+  get '/about', to: 'static_pages#about'
 end
---
+++ db/migrate/20200521131258_add_details_to_users.rb
@@ -0,0 +1,6 @@
+class AddDetailsToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :profile, :text
+    add_column :users, :admin, :boolean, default: false, null: false
+  end
+end
