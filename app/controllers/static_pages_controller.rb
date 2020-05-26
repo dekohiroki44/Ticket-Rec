6d31e99ec4f9cd04ea5e517bb64d811a8473d6e4
dekohiroki44 <drinkingrider@hotmail.co.jp>
2020-05-26 20:13:29 +0900

    paginationを実装

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
-    @events = current_user.events
+
+    @events = current_user.events.page(params[:page]) if user_signed_in?
   end
 
   def about
