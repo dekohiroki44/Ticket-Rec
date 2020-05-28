414fb4f57fbc50b15e053913bb3ad5028cc1b61c
dekohiroki44 <drinkingrider@hotmail.co.jp>
2020-05-28 20:13:22 +0900

    フォロー機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_22_101651) do
+ActiveRecord::Schema.define(version: 2020_05_28_010341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,16 @@ ActiveRecord::Schema.define(version: 2020_05_22_101651) do
     t.datetime "updated_at", null: false
     t.index ["date"], name: "index_events_on_date"
     t.index ["user_id"], name: "index_events_on_user_id"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
   create_table "users", force: :cascade do |t|
