9bf5e73f3ac0e51f2d197add79c801d98b46bb16
dekohiroki44 <drinkingrider@hotmail.co.jp>
2020-05-31 20:49:51 +0900

    通知機能の実装

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :event
   validates :content, presence: true
+  has_many :notifications, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,8 @@ class User < ApplicationRecord
   has_many :followers, through: :passive_relationships, source: :follower
   has_many :likes, dependent: :destroy
   has_many :comments
+  has_many :active_notifications, class_name: 'Notification', foreign_key: 'visitor_id', dependent: :destroy
+  has_many :passive_notifications, class_name: 'Notification', foreign_key: 'visited_id', dependent: :destroy
 
   def feed
     Event.where("user_id = ?", id)
@@ -33,6 +35,17 @@ class User < ApplicationRecord
 
   def following?(other_user)
     following.include?(other_user)
+  end
+
+  def create_notification_follow!(current_user)
+    temp = Notification.where(["visitor_id = ? and visited_id = ? and action = ? ", current_user.id, id, 'follow'])
+    if temp.blank?
+      notification = current_user.active_notifications.new(
+        visited_id: id,
+        action: 'follow'
+      )
+      notification.save if notification.valid?
+    end
   end
 
 
