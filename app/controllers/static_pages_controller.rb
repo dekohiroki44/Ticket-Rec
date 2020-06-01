525a6d87d5260e155994153c753101dd8b0c7a3d
dekohiroki44 <drinkingrider@hotmail.co.jp>
2020-06-01 23:43:47 +0900

    events/new,editのformを編集

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
     if user_signed_in?
-      @done_events = current_user.events.done.page(params[:page]).with_attached_images.includes(likes: :user)
-      @upcomming_events = current_user.events.upcomming.page(params[:page]).with_attached_images.includes(likes: :user)
-      @unsolved_events = current_user.events.unsolved.page(params[:page]).with_attached_images.includes(likes: :user)
+      @done_events = current_user.events.done.page(params[:page]).with_attached_images
+      @upcomming_events = current_user.events.upcomming.page(params[:page]).with_attached_images
+      @unsolved_events = current_user.events.unsolved.page(params[:page]).with_attached_images
     end
   end
 
