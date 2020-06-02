caf99a543da348a08ad635f6c37a8cd0c8be63be
dekohiroki44 <drinkingrider@hotmail.co.jp>
2020-06-02 21:18:11 +0900

    recommendメソッドの作成

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
     if user_signed_in?
-      @done_events = current_user.events.done.page(params[:page]).with_attached_images
-      @upcomming_events = current_user.events.upcomming.page(params[:page]).with_attached_images
-      @unsolved_events = current_user.events.unsolved.page(params[:page]).with_attached_images
+      @done_events = current_user.events.done.page(params[:page]).with_attached_images.includes([:like_users])
+      @upcomming_events = current_user.events.upcomming.page(params[:page]).with_attached_images.includes([:like_users])
+      @unsolved_events = current_user.events.unsolved.page(params[:page]).with_attached_images.includes([:like_users])
     end
   end
 
