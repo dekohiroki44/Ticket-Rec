c771b1a30d9f90ce21d9783952318d04351dea0e
dekohiroki44 <drinkingrider@hotmail.co.jp>
2020-07-03 22:02:32 +0900

    spotifyメソッドを修正、tickets/showを修正

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,11 @@
 require File.expand_path(File.dirname(__FILE__) + "/environment")
 
+env :PATH, ENV['PATH']
 rails_env = Rails.env.to_sym
 rails_root = Rails.root.to_s
 set :environment, rails_env
 set :output, "#{rails_root}/log/cron.log"
+ENV.each { |k, v| env(k, v) }
 
 every 1.days, at: '14:45 am' do
   runner "UserHelper.set_mail"
--- spec/systems/signup_spec.rb
+++ spec/systems/signup_spec.rb
@@ -33,6 +33,7 @@ describe 'signup', type: :system do
     expect(page).to have_link '皆んなのチケット', href: tickets_path
     expect(page).to have_link '書き込む', href: new_ticket_path
     expect(page).to have_link '通知', href: notifications_path
+    expect(page).to have_link 'Ticket Recについて', href: about_path
     expect(page).to have_link 'ログアウト', href: destroy_user_session_path
     expect(page).not_to have_link 'ログイン', href: new_user_session_path
   end
