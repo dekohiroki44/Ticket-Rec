e99c5857a71f79d32bd9a241f54606dc41dd7228
dekohiroki44 <drinkingrider@hotmail.co.jp>
2020-07-05 22:11:54 +0900

    Add_for_deploy_by_capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,2 @@
-# EC2サーバーのIP、EC2サーバーにログインするユーザー名、サーバーのロールを記述
-server 'ticket-rec.com', user: 'hiroki', roles: %w{app db web}
-
-# デプロイするサーバーにsshログインする鍵の情報を記述
+server '54.95.141.117', user: 'hiroki', roles: %w{app db web}
 set :ssh_options, keys: '~/.ssh/ticket-rec_key_rsa'
