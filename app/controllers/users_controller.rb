d0e43001063e9a2678bd0d710c588a250fd4ca10
dekohiroki44 <drinkingrider@hotmail.co.jp>
2020-07-10 16:59:08 +0900

    環境変数POSTGRES_HOSTを定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:show, :destroy, :following, :followers]
+  before_action :set_user
   before_action :admin_user, only: :destroy
 
   def show
