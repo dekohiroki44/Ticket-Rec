48cddec89c8f065904cf999eaa80068abd6ff1e5
dekohiroki44 <drinkingrider@hotmail.co.jp>
2020-07-11 15:21:28 +0900

    wheneverの設定を修正

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + "/environment")
 
 env :PATH, ENV['PATH']
-# set :environment, :development
+set :environment, :production
 set :output, 'log/cron.log'
 ENV.each { |k, v| env(k, v) }
 
