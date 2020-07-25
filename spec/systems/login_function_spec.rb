4c538995cfa271d47a6dd9e5e20fdb8e88e90605
dekohiroki44 <drinkingrider@hotmail.co.jp>
2020-07-25 21:18:10 +0900

    login_function_specにspecを追記

--- spec/systems/login_function_spec.rb
+++ spec/systems/login_function_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'login', type: :system do
+describe 'login_function', type: :system do
   before do
     visit new_user_session_path
   end
@@ -34,7 +34,7 @@ describe 'login', type: :system do
       click_button 'ログイン'
     end
 
-    it 'show correct links' do
+    it 'shows correct links' do
       expect(page).to have_link 'マイページ', href: user_path(user)
       expect(page).to have_link '皆んなのチケット', href: tickets_path
       expect(page).to have_link '書き込む', href: new_ticket_path
@@ -53,6 +53,45 @@ describe 'login', type: :system do
       expect(page).to have_link '新規登録', href: new_user_registration_path
       expect(page).to have_link 'ログイン', href: new_user_session_path
       expect(page).not_to have_link 'マイページ', href: user_path(user)
+      expect(page).not_to have_link '皆んなのチケット', href: tickets_path
+      expect(page).not_to have_link '書き込む', href: new_ticket_path
+      expect(page).not_to have_link '通知', href: notifications_path
+      expect(page).not_to have_link 'Ticket Recについて', href: about_path
+      expect(page).not_to have_link 'ログアウト', href: destroy_user_session_path
+    end
+  end
+
+  context 'when cllick guest login' do
+    before do
+      within '#sidebar-wrapper' do
+        click_link 'ゲストログイン（閲覧用）'
+      end
+    end
+
+    it 'creates guest user' do
+      expect(User.count).to eq 1
+      expect(User.last.name).to eq 'ゲストユーザー'
+    end
+
+    it 'shows correct links' do
+      expect(page).to have_link 'マイページ', href: user_path(User.last)
+      expect(page).to have_link '皆んなのチケット', href: tickets_path
+      expect(page).to have_link '書き込む', href: new_ticket_path
+      expect(page).to have_link '通知', href: notifications_path
+      expect(page).to have_link 'Ticket Recについて', href: about_path
+      expect(page).to have_link 'ログアウト', href: destroy_user_session_path
+      expect(page).not_to have_link '新規登録', href: new_user_registration_path
+      expect(page).not_to have_link 'ログイン', href: new_user_session_path
+    end
+
+    it 'has logout function' do
+      within '#sidebar-wrapper' do
+        click_link 'ログアウト'
+      end
+      expect(current_path).to eq root_path
+      expect(page).to have_link '新規登録', href: new_user_registration_path
+      expect(page).to have_link 'ログイン', href: new_user_session_path
+      expect(page).not_to have_link 'マイページ', href: user_path(User.last)
       expect(page).not_to have_link '皆んなのチケット', href: tickets_path
       expect(page).not_to have_link '書き込む', href: new_ticket_path
       expect(page).not_to have_link '通知', href: notifications_path
