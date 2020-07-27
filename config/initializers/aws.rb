53a442867dbc6bc34313148c7faa1fe480adc122
dekohiroki44 <drinkingrider@hotmail.co.jp>
2020-07-27 22:35:47 +0900

    initializers/aws.rbを削除

--- config/initializers/aws.rb
+++
@@ -1,5 +0,0 @@
-ActionMailer::Base.add_delivery_method :ses,
-                                       AWS::SES::Base,
-                                       access_key_id: "AKIAX5LZT23PUXYSSVPH",
-                                       secret_access_key: "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",
-                                       server: 'email.us-west-2.amazonaws.com'
